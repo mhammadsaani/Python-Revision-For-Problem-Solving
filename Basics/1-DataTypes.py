a487735667cd5f688ef320339708daf12ae12221
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2022-03-29 21:54:30 +0500

    Basics Added

---
+++ Basics/1-DataTypes.py
@@ -0,0 +1,34 @@
+# Numbers
+
+# - Integers
+a = 5
+print(type(a))\
+
+# - Floats
+b = 3.45
+print(type(b))
+
+# Complex
+c = complex(2, 3)
+print(c)
+print(type(c))
+
+
+# Boolean
+f_bool = False
+print(type(f_bool))
+
+t_bool = True
+print(type(t_bool))
+
+
+# String 
+
+singleQuote = 'Hey, I am Single Quote'
+doubleQuote = "Hey, I am Double Quote"
+tripleQuote = """Hey, I am triple Quote"""
+char = '$'
+print(singleQuote, doubleQuote, tripleQuote, char)
+
+print(len(singleQuote))
+print("Hey , I am 5th index in singleQuote", singleQuote[5])

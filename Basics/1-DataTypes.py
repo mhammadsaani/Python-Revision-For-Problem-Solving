d6c92bdf0d648652e1d514a80061dee4c4cb1733
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2022-03-31 21:21:02 +0500

    Day-2 Added

--- Basics/1-DataTypes.py
+++ Basics/1-DataTypes.py
@@ -84,4 +84,76 @@ print("%i + %i" %(2, 3))
 
 print("%.2f + %.2f" %(2.3, 5.3))
 
+### Important, When two strings have more values, string which comes first in dictionary has smaller value
 
+
+# Conditional
+
+num = 5
+if (num == 5):
+  print("Hey")
+else:
+  print("Bye")
+
+
+
+# def calculator(operation, n1, n2):
+#     return operation(n1, n2)  # Using the 'operation' argument as a function
+
+
+# # 10 and 20 are the arguments.
+# result = calculator(lambda n1, n2: n1 * n2, 10, 20)
+# # The lambda multiplies them.
+# print(result)
+
+# print(calculator(lambda n1, n2: n1 + n2, 10, 20))
+
+
+# operation = lambda n1, n2: n1*n2
+
+# print(operation(10, 20))
+
+# def calculator(operation, n1, n2):
+#   return operation(n1, n2)
+
+
+# print(calculator(operation, 10, 20))
+
+
+llist = [1, 2, 3, 4, 5]
+
+doubleList = map(lambda n: n * 2, llist)
+print(list(doubleList))
+
+greaterThree = list(filter(lambda n: n > 2, llist))
+print(greaterThree)
+
+
+def factorial(n):
+    if (n == 2):
+        return 2
+    return n * factorial(n-1)
+
+print(factorial(5))
+
+x = [1, 2, 3, 4]
+for i in range(len(x)):
+  print(x[i])
+
+
+for i in x:
+  print(i)
+
+
+def fib(n):
+  if n < 3:
+    return 1
+  fristNum = 0
+  secondNum = 1
+  finalAns = 0
+  for i in range(n - 2):
+    temp = fristNum + secondNum
+    fristNum = secondNum
+    secondNum = temp
+    finalAns = temp
+  return finalAns

9856e9edd2b399c0b60819a5d050ba7e6db67272
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2022-04-09 13:24:12 +0500

    Test Commit

--- 3- Problem Solving/1- Arrays/test.py
+++ 3- Problem Solving/1- Arrays/test.py
@@ -1,2 +1,21 @@
-a = 'abc' * 2.5
-print(a)
+def ArrayChallenge(arr):
+  arrToken = "r4vc9ywieb1"
+  ans = []
+  
+  if len(arr) > 3:
+    ans =  [arr[1], arr[-2]]
+  else:
+    ans =  [arr[0], arr[-1]]
+
+  temp = str(ans[0]) + "X" + str(ans[1])
+  tempLen = len(temp)
+  arrToken = temp + arrToken
+  tempStr = ""
+  for i in range(tempLen + 1, len(arrToken), 2):
+    tempStr = arrToken.replace(str(arrToken[i]), "X", 1)
+    
+  return tempStr
+
+
+# keep this function call here 
+print(ArrayChallenge(input()))
